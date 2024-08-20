e018c4f48417239abb9678d9c58a66fa767dc76c
soham603 <soham.lad16793@sakec.ac.in>
2024-08-20 13:40:41 +0530

    WEb Api Commit into api folder

--- WebAPI_Backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ WebAPI_Backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43d3ccab9d6282c3ca301a8cbd405de45dc33e14")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
